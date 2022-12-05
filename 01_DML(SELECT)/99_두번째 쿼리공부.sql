50b5cbe78eb51b4e7a31d7711ad7a60dfffbfc8f
KIMSUJIN98 <qwer9402@naver.com>
2022-12-05 09:18:46 +0900

    kh_edu
    Oracle 2022-12-05

--- 01_DML(SELECT)/99_두번째 쿼리공부.sql
+++ 01_DML(SELECT)/99_두번째 쿼리공부.sql
@@ -9,7 +9,6 @@ ORDER BY SALARY DESC; -- 4
 
 -- 어떤 문제점이 있는지, 해결된 SQL문 작성!!!
 -- 문제점 : 정렬이 되기도 전에 ROWNUM이 부여됨. 데이터가 이상하게 현출됨
-
 SELECT ROWNUM, EMP_NAME, SALARY
 FROM (SELECT *
       FROM EMPLOYEE
@@ -32,10 +31,85 @@ GROUP BY DEPT_CODE
 ORDER BY 1;
 
 -- 어떤 문제점이 있는지, 해결된 SQL문 작성!!!
-
 SELECT DEPT_CODE, SUM(SALARY) 총합, FLOOR(AVG(SALARY)) 평균, COUNT(*) 인원수
 FROM EMPLOYEE
 GROUP BY DEPT_CODE
 HAVING AVG(SALARY) > 2700000
 ORDER BY 1;
+
+--------------------------------------------------------------------------------
+-- 서술형대비
+-- JOIN 종류 (내부조인, 외부조인, 등등...)별 특징, 역할
+-- 함수 종류(TRIM, ...) 별 각각의 역할
+
+-- 직원의 급여 조회시 각 직급별로 인상해서 조회
+-- J7인 사원은 급여를 10% 인상 (SALARY * 1.1)
+-- J6인 사원은 급여를 15% 인상 (SALARY * 1.15)
+-- J5인 사원은 급여를 20% 인상 (SALARY * 1.2)
+-- 그 외의 사원은 급여를 5% 인상 (SALARY * 1.05)
+
+SELECT EMP_NAME, JOB_CODE, SALARY,
+       DECODE(JOB_CODE, 'J7', SALARY * 1.1,
+                        'J6', SALARY * 1.15,
+                        'J5', SALARY * 1.2,
+                        SALARY * 1.05) AS "인상된 급여"
+FROM EMPLOYEE;
+
+-- '21/09/28' 와 같은 문자열을 가지고 '2021-09-28'로 표현해보기
+-- '210908'와 같은 문자열을 가지고 2021년 9월 8일 표현(구글링 가능) -- 09월 08일이 아님
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
