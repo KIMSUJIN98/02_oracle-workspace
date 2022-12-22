62b7111af37df3e86c0b7de19d05ba439f5d762b
KIMSUJIN98 <qwer9402@naver.com>
2022-12-22 17:58:44 +0900

    kh_edu
    221223_test

--- 99_문제들/쿼리공부/99_네번째 쿼리공부.sql
+++ 99_문제들/쿼리공부/99_네번째 쿼리공부.sql
@@ -1,10 +1,13 @@
--- 1 문제해결 시나리오
+-- 1. 문제해결 시나리오
 SELECT DEPT_CODE, JOB_CODE, SALARY
 FROM EMPLOYEE
 GROUP BY DEPT_CODE;
--- JOB_CODE가 그룹 바이로 묶여있지 않고, SALARY가 그룹 바이로 묶여있지 않으면서 그룹함수로도 씌워져 있지 않음.
+-- 원인
+-- JOB_CODE가 GROUP BY로 묶여있지 않고, SALARY가 GROUP BY로 묶여있지 않으면서 그룹함수로도 씌여 있지 않음.
+-- SELECT 목록이 GROUP BY와 일치하지 않으므로, 수정이 필요함.
 
 SELECT DEPT_CODE, JOB_CODE, SUM(SALARY)
 FROM EMPLOYEE
 GROUP BY DEPT_CODE, JOB_CODE;
+-- 해결방안
 -- 그룹으로 묶은것은 반드시 SELECT절에 나오거나, 그룹함수로 묶여야 한다.
